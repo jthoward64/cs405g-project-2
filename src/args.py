0eb81d17cc74b924114726cd9b4f1c539ef51104
Joshua Tag Howard <tag@jthoward.dev>
2023-11-27 12:27:16 -0500

    Refactor argparse in args.py

--- src/args.py
+++ src/args.py
@@ -1,8 +1,7 @@
 import argparse
 
-parser = (
-    argparse.ArgumentParser(description="PyTorch ImageNet Training")
-    .add_argument("--password", type=str, help="Database password")
-)
+parser = argparse.ArgumentParser(description="PyTorch ImageNet Training")
+parser.add_argument("--password", type=str, help="Database password")
+
 
 args = parser.parse_args()
