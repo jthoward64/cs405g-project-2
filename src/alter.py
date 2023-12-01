6bfb277876083b419f1fbc48972f480d8fad547c
Joshua Tag Howard <tag@jthoward.dev>
2023-11-30 22:00:08 -0500

    Add column for contact preferences and fix column name typo

--- src/alter.py
+++ src/alter.py
@@ -4,11 +4,11 @@ if not connection:
     print("Could not connect - no connection object")
 elif connection.is_connected():
     with connection.cursor() as cursor:
+        # cursor.execute(
+        #     "ALTER TABLE Entity_Table ADD COLUMN (Contact_Preferences TINYINT UNSIGNED NOT NULL DEFAULT 0);"
+        # )
         cursor.execute(
-            "ALTER TABLE Entity_Table ADD COLUMN (Contact_Preferences TINYINT UNSIGNED NOT NULL DEFAULT 0);"
-        )
-        cursor.execute(
-            "ALTER TABLE Entity_Table ADD COLUMN (duplicates VARCHAR(1) NOT NULL DEFAULT 'N');"
+            "ALTER TABLE Entity_Table ADD COLUMN (duplicate VARCHAR(1) NOT NULL DEFAULT 'N');"
         )
 
     connection.commit()
