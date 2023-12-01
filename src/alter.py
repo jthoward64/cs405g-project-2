1400eef3995924de4e0d6d337e6c82d85cb2642e
Joshua Tag Howard <tag@jthoward.dev>
2023-11-30 22:40:29 -0500

    Add primary key and foreign key constraint to Entity_Table

--- src/alter.py
+++ src/alter.py
@@ -7,9 +7,22 @@ elif connection.is_connected():
         # cursor.execute(
         #     "ALTER TABLE Entity_Table ADD COLUMN (Contact_Preferences TINYINT UNSIGNED NOT NULL DEFAULT 0);"
         # )
+        # cursor.execute(
+        #     "ALTER TABLE Entity_Table ADD COLUMN (duplicate VARCHAR(1) NOT NULL DEFAULT 'N');"
+        # )
+
         cursor.execute(
-            "ALTER TABLE Entity_Table ADD COLUMN (duplicate VARCHAR(1) NOT NULL DEFAULT 'N');"
+            "ALTER TABLE Entity_Table DROP FOREIGN KEY fk_Telephone_Number, DROP INDEX fk_Telephone_Number;"
         )
+        connection.commit()
+        cursor.execute(
+            "ALTER TABLE Telephone_Numbers DROP primary key, ADD primary key(Telephone_Number, Entity_ID);"
+        )
+        print("Added Primary Key")
+        cursor.execute(
+            "ALTER TABLE Entity_Table ADD CONSTRAINT fk_Telephone_Number FOREIGN KEY (Entity_ID, Primary_Telephone_Number) REFERENCES Telephone_Numbers(Entity_ID, Telephone_Number);"
+        )
+        print("Added Foreign Key Constraint")
 
     connection.commit()
     connection.close()
