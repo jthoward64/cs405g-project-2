e8457d1539e532eb479f2cf5d4dc6bdfee755985
Joshua Tag Howard <tag@jthoward.dev>
2023-11-30 21:48:26 -0500

    Use connection.py for deliverables 5 and 6

--- src/Deliverable_5.py
+++ src/Deliverable_5.py
@@ -1,46 +1,54 @@
 import mysql.connector
 import pandas as pd
+
 from args import args
+from connection import connection
 
-#Connect to mysql
-cnx = mysql.connector.connect(user = args.linkblue,
-                              password = args.password,
-                              host = mysql.cs.uky.edu,
-                              database = args.linkblue)
 # Create Dataframe
-Entity_Table = pd.read_sql('Select * from Entity_Table_3', cnx)
+Entity_Table = pd.read_sql("Select * from Entity_Table", connection)
 
-Entity_Table['duplicate'] = 'N'
+Entity_Table["duplicate"] = "N"
 
-Entity_Table.loc[:5, 'duplicate'] = 'Y'
+Entity_Table.loc[:5, "duplicate"] = "Y"
 
-#Create duplicates dataframe
-duplicates = Entity_Table[Entity_Table.duplicate == 'Y'].copy()
-duplicates.reset_index(inplace = True, drop = True)
+# Create duplicates dataframe
+duplicates = Entity_Table[Entity_Table.duplicate == "Y"].copy()
+duplicates.reset_index(inplace=True, drop=True)
 
-#Combine name and Address to get identifier for possible duplicates
-duplicates["Name_Address"] = duplicates.EntityName + " " + duplicates.Street_Name + " " + duplicates.City + " " + duplicates.StateName + " " + duplicates.Zip
+# Combine name and Address to get identifier for possible duplicates
+duplicates["Name_Address"] = (
+    duplicates.EntityName
+    + " "
+    + duplicates.Street_Name
+    + " "
+    + duplicates.City
+    + " "
+    + duplicates.StateName
+    + " "
+    + duplicates.Zip
+)
 unique_names = duplicates.Name_Address.unique()
 
 # Create postcard
-# Only send postcard to first listing for likely duplicate. If after sending postcard it is determined that the customer was not a duplicate, 
+# Only send postcard to first listing for likely duplicate. If after sending postcard it is determined that the customer was not a duplicate,
 # adjust database to indicate that the likely duplicates were unique customers
 for i in unique_names:
     row = duplicates[duplicates.Name_Address == i].iloc[0]
-    print(row.EntityName + 
-          '\n' + 
-          row.Street_Name +
-          '\n' +
-          row.City +
-          ', ' +
-          row.StateName +
-          ' ' +
-          row.Zip +
-          '\n\n' +
-          row.EntityName +
-          '\n' +
-          'It is great to meet you again! Your favorite food delivery family is growing, looks like you have already met some of our now expanded family. We want you to know that you will now get the same great service from our newly added drivers!' +
-          '\n\n' +
-          'Looking forward to serving you again!' +
-          '\n'
-          )
+    print(
+        row.EntityName
+        + "\n"
+        + row.Street_Name
+        + "\n"
+        + row.City
+        + ", "
+        + row.StateName
+        + " "
+        + row.Zip
+        + "\n\n"
+        + row.EntityName
+        + "\n"
+        + "It is great to meet you again! Your favorite food delivery family is growing, looks like you have already met some of our now expanded family. We want you to know that you will now get the same great service from our newly added drivers!"
+        + "\n\n"
+        + "Looking forward to serving you again!"
+        + "\n"
+    )
--- src/Deliverable_6.py
+++ src/Deliverable_6.py
@@ -1,111 +1,101 @@
 import mysql.connector
 import pandas as pd
+
+from connection import connection
 from contact import get_contact_info
-from args import args
 
-
-#Connect to mysql
-cnx = mysql.connector.connect(user = args.linkblue,
-                              password = args.password,
-                              host = 'mysql.cs.uky.edu',
-                              database = args.linkblue)
 # Create Dataframe
-Entity_Table = pd.read_sql('Select * from Entity_Table_3', cnx)
+Entity_Table = pd.read_sql("Select * from Entity_Table", connection)
 
-#Create duplicates dataframe
-duplicates = Entity_Table[Entity_Table.duplicate == 'Y'].copy()
-duplicates.reset_index(inplace = True, drop = True)
+# Create duplicates dataframe
+duplicates = Entity_Table[Entity_Table.duplicated == "Y"].copy()
+duplicates.reset_index(inplace=True, drop=True)
 
-#Combine name and Address to get identifier for possible duplicates
-duplicates["Name_Address"] = duplicates.EntityName + " " + duplicates.Street_Name + " " + duplicates.City + " " + duplicates.StateName + " " + duplicates.Zip
+# Combine name and Address to get identifier for possible duplicates
+duplicates["Name_Address"] = (
+    duplicates.EntityName
+    + " "
+    + duplicates.Street_Name
+    + " "
+    + duplicates.City
+    + " "
+    + duplicates.StateName
+    + " "
+    + duplicates.Zip
+)
 unique_names = duplicates.Name_Address.unique()
 
-# Send Coupon codes. Our customers are listed as entities rather than as businesses and customers; therefore, entity name is 
+# Send Coupon codes. Our customers are listed as entities rather than as businesses and customers; therefore, entity name is
 # used for the name for the coupon.
 # Customer can choose a contact preference that involves a phone number without providing a phone number. If no phone number is provided, the business is alerted that there is no phone number and therefore can collect one.
 for i in unique_names:
     row = duplicates[duplicates.Name_Address == i].iloc[0]
     row.contact_preferences = get_contact_info(row.contact_preferences)
-    if row.contact_preferences['email'] == 'True':
-        print('EMAIL: ' +
-            row.EntityName +
-            ';' +
-             row.email +
-            ';' +
-            '25% Coupon Code'
+    if row.contact_preferences["email"] == "True":
+        print("EMAIL: " + row.EntityName + ";" + row.email + ";" + "25% Coupon Code")
+    if row.contact_preferences["text"] == True:
+        if row.Primary_Telephone_Number != None:
+            print(
+                "TEXT: "
+                + row.EntityName
+                + "; "
+                + row.Primary_Telephone_Number
+                + "; "
+                + "25% Coupon Code"
             )
-    if row.contact_preferences['text'] == True:
-        if row.Primary_Telephone_Number != None:
-            print('TEXT: ' +
-                row.EntityName +
-                '; ' +
-                 row.Primary_Telephone_Number +
-                '; ' +
-                '25% Coupon Code'
-                )
         else:
-            print('TEXT: ' +
-                row.EntityName +
-                '; No phone number provided'
-                )
-    if row.contact_preferences['robocall'] == 'True':
+            print("TEXT: " + row.EntityName + "; No phone number provided")
+    if row.contact_preferences["robocall"] == "True":
         if row.Primary_Telephone_Number != None:
-            print('ROBOCALL: ' +
-                row.EntityName +
-                '; ' +
-                 row.Primary_Telephone_Number +
-                '; ' +
-                '25% Coupon Code'
-                )
-        else:
-            print('ROBOCALL: ' +
-                row.EntityName +
-                '; No phone number provided'
-                )
-    if row.contact_preferences['phone'] == True:
-        if row.Primary_Telephone_Number != None:
-            print('PHONE: ' +
-                row.EntityName +
-                '; ' +
-                 row.Primary_Telephone_Number +
-                '; ' +
-                '25% Coupon Code'
-                )
-        else:
-            print('PHONE: ' +
-                row.EntityName +
-                '; No phone number provided'
-                )
-    if row.contact_preferences['fax'] == True:
-        if row.Primary_Telephone_Number != None:
-            print('FAX: ' +
-                row.EntityName +
-                '; ' +
-                 row.Primary_Telephone_Number +
-                '; ' +
-                '25% Coupon Code'
-                )
-        else:
-            print('FAX: ' +
-                row.EntityName +
-                '; No phone number provided'
-                )
-    if row.contact_preferences['dnc'] == True:
-        print('DNC: ' +
-            row.EntityName 
+            print(
+                "ROBOCALL: "
+                + row.EntityName
+                + "; "
+                + row.Primary_Telephone_Number
+                + "; "
+                + "25% Coupon Code"
             )
-    if row.contact_preferences['mail'] == True:
-        print('MAIL: ' +
-            row.EntityName +
-            '; ' +
-             row.Street_Name +
-             ' ' +
-             row.City +
-             ', ' +
-             row.StateName +
-             ' ' +
-             row.Zip +
-            '; ' +
-            '25% Coupon Code'
+        else:
+            print("ROBOCALL: " + row.EntityName + "; No phone number provided")
+    if row.contact_preferences["phone"] == True:
+        if row.Primary_Telephone_Number != None:
+            print(
+                "PHONE: "
+                + row.EntityName
+                + "; "
+                + row.Primary_Telephone_Number
+                + "; "
+                + "25% Coupon Code"
             )
-    print(' ')
+        else:
+            print("PHONE: " + row.EntityName + "; No phone number provided")
+    if row.contact_preferences["fax"] == True:
+        if row.Primary_Telephone_Number != None:
+            print(
+                "FAX: "
+                + row.EntityName
+                + "; "
+                + row.Primary_Telephone_Number
+                + "; "
+                + "25% Coupon Code"
+            )
+        else:
+            print("FAX: " + row.EntityName + "; No phone number provided")
+    if row.contact_preferences["dnc"] == True:
+        print("DNC: " + row.EntityName)
+    if row.contact_preferences["mail"] == True:
+        print(
+            "MAIL: "
+            + row.EntityName
+            + "; "
+            + row.Street_Name
+            + " "
+            + row.City
+            + ", "
+            + row.StateName
+            + " "
+            + row.Zip
+            + "; "
+            + "25% Coupon Code"
+        )
+    print(" ")
