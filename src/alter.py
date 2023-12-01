154662dd9efdd77700234da37c2ab59ecf4f2858
Joshua Tag Howard <tag@jthoward.dev>
2023-11-30 22:46:43 -0500

    Add email column to Entity_Table

--- src/alter.py
+++ src/alter.py
@@ -11,18 +11,20 @@ elif connection.is_connected():
         #     "ALTER TABLE Entity_Table ADD COLUMN (duplicate VARCHAR(1) NOT NULL DEFAULT 'N');"
         # )
 
-        cursor.execute(
-            "ALTER TABLE Entity_Table DROP FOREIGN KEY fk_Telephone_Number, DROP INDEX fk_Telephone_Number;"
-        )
-        connection.commit()
-        cursor.execute(
-            "ALTER TABLE Telephone_Numbers DROP primary key, ADD primary key(Telephone_Number, Entity_ID);"
-        )
-        print("Added Primary Key")
-        cursor.execute(
-            "ALTER TABLE Entity_Table ADD CONSTRAINT fk_Telephone_Number FOREIGN KEY (Entity_ID, Primary_Telephone_Number) REFERENCES Telephone_Numbers(Entity_ID, Telephone_Number);"
-        )
-        print("Added Foreign Key Constraint")
+        # cursor.execute(
+        #     "ALTER TABLE Entity_Table DROP FOREIGN KEY fk_Telephone_Number, DROP INDEX fk_Telephone_Number;"
+        # )
+        # connection.commit()
+        # cursor.execute(
+        #     "ALTER TABLE Telephone_Numbers DROP primary key, ADD primary key(Telephone_Number, Entity_ID);"
+        # )
+        # print("Added Primary Key")
+        # cursor.execute(
+        #     "ALTER TABLE Entity_Table ADD CONSTRAINT fk_Telephone_Number FOREIGN KEY (Entity_ID, Primary_Telephone_Number) REFERENCES Telephone_Numbers(Entity_ID, Telephone_Number);"
+        # )
+        # print("Added Foreign Key Constraint")
+
+        cursor.execute("ALTER TABLE Entity_Table ADD COLUMN (email VARCHAR(255));")
 
     connection.commit()
     connection.close()
